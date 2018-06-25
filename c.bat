30af46e8e951dd3bc9a6e8740e1c851c9cddb292
strengthtoendure <francisco.bahamondes90@gmail.com>
2018-06-24 22:19:26 -0400

    test

--- c.bat
+++ c.bat
@@ -1,3 +1,5 @@
+if not exist bin\Debug md bin\Debug
+if not exist obj\Debug/ md obj\Debug
 g++ -Wall -fexceptions -g -ISFML-2.5.0/include -c Game.cpp -o obj/Debug/Game.o
 g++ -Wall -fexceptions -g -ISFML-2.5.0/include -c Main.cpp -o obj/Debug/Main.o
 g++ -Wall -fexceptions -g -ISFML-2.5.0/include -c Window.cpp -o obj/Debug/Window.o
